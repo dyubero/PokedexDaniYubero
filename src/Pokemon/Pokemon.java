343fd59f0a7843537e1b5f29a9d2b35cacdff214
dyubero <dyubero96@gmail.com>
2015-05-08 13:14:48 +0200

    falta modo auto

---
+++ src/Pokemon/Pokemon.java
@@ -0,0 +1,17 @@
+
+package Pokemon;
+
+/**
+ *
+ * @author Jorge Cisneros
+ */
+public class Pokemon {
+    public String nombre ="";
+    public int generation_id = 0;
+    public int evolution_chain_id = 0;
+    public String species="";
+    public String height="";
+    public String weight="";
+    public String habitat="";
+    public String rango="";
+}
